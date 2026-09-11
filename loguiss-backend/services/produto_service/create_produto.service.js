f61de7cc532876c9f160ae5997ee4437531293fd
Ykwim-77 <christian.darosa0106@gmail.com>
2026-09-11 18:53:12 -0300

    fix: list_produtos correct

--- loguiss-backend/services/produto_service/create_produto.service.js
+++ loguiss-backend/services/produto_service/create_produto.service.js
@@ -6,6 +6,20 @@ import create_estoque_service from '../estoque_service/create_entoque.service.js
 
 
 export async function create_produto_service(req, res) {
+    let { descricao, 
+        id_tipo_produto, 
+        id_unidade_medida, 
+        is_fracionado, 
+        valor, 
+        flag_ativo,
+        id_categoria_user,
+        id_receita,
+        id_fornecedor_user,
+        quantidade,
+        dt_entrada,
+        minimo,
+        prazo_saida } = req.body
+
 
     let campos_obrigatorios = [
         descricao, 
@@ -28,6 +42,25 @@ export async function create_produto_service(req, res) {
         id_unidade_medida = Number(id_unidade_medida);
         id_categoria_user = Number(id_categoria_user);
         id_fornecedor_user = Number(id_fornecedor_user);
+
+        const existe_fornecedor = await prisma.fornecedor.findUnique({
+            where:{
+                id_fornecedor: id_fornecedor_user
+            }
+        })
+        const existe_categoria = await prisma.categoria.findUnique({
+            where:{
+                id_categoria: id_categoria_user
+            }
+        })
+
+        if(!existe_fornecedor){
+            return res.status(400).json({MSG: "favor informar um fornecedor válido!"})
+        }
+
+        if(!existe_categoria){
+            return res.status(400).json({MSG: "favor informar uma categoria válida!"})
+        }
         
         const produto = await prisma.produto.create({
             data:{
@@ -46,20 +79,28 @@ export async function create_produto_service(req, res) {
             },
             include:{
                 categorias: true,
-                fornecedores: true,
+                fornecedores: {
+                    include: {
+                        fornecedor: true
+                    }
+                },
                 unidade_medida: true
             }
         })
         const id_produto = produto.id_produto
         const id_categoria = id_categoria_user
+        
         const object_link = await link_categoria_produto(id_categoria, id_produto);
-
         console.log(object_link)
 
         const link_fornecedor_produto = await link_fornecedor_produto_service(quantidade, id_fornecedor_user, id_produto, req, res)
         console.log(link_fornecedor_produto)
+
+
         const link_estoque_produto = await create_estoque_service(dt_entrada, id_produto, is_fracionado, minimo, prazo_saida, quantidade)
         console.log("estoque criada e vinculado!", link_estoque_produto)
+
+
         return res.status(200).json({MSG:"produto criado com sucesso!!", produto: produto})
     } catch (error) {
         console.log(error)
