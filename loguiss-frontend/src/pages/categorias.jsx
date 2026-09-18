3b1b3bbac778203d8315ebcf267b52eaaf3cac2b
Ykwim-77 <christian.darosa0106@gmail.com>
2026-09-17 22:16:59 -0300

    Fix import paths for components in categorias.jsx

--- loguiss-frontend/src/pages/categorias.jsx
+++ loguiss-frontend/src/pages/categorias.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import { toast } from 'sonner';
 
 import { Search, Layers2 } from 'lucide-react';
-import { SideBar } from '../components/Sidebar';
-import { Button } from '../components/Button'
-import { Inputs } from '../components/Inputs';
-import { Card } from '../components/Card';
+import { SideBar } from '../components/sidebar';
+import { Button } from '../components/button'
+import { Inputs } from '../components/inputs';
+import { Card } from '../components/card';
 
 import api from '../services/api';
 
