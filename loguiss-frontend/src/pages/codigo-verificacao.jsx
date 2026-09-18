a5f15783f22de1690efb0f34c2b1ac395df1775f
Ykwim-77 <christian.darosa0106@gmail.com>
2026-09-17 22:17:08 -0300

    Fix import case for Button component

--- loguiss-frontend/src/pages/codigo-verificacao.jsx
+++ loguiss-frontend/src/pages/codigo-verificacao.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '../components/Button';
+import { Button } from '../components/button';
 
 import api from '../services/api.js'
 
